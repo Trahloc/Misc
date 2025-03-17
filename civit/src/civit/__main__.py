c88197ed231d243aa481bdc1b8f61b1dd1355a54
Trahloc colDhart <github@trahloc.com>
2025-03-17 20:17:34 +0800

    ok, now it's fixed

--- civit/src/civit/__main__.py
+++ civit/src/civit/__main__.py
@@ -7,7 +7,11 @@
 ## DEPENDENCIES:
  - civit.cli
 """
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 from civit.cli import main
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
